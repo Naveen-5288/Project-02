00fb9b368ccc1d0ab6c88f73bbe04b6a8f4ec367
Naveen Harikoppa <217095533+Naveen-5288@users.noreply.github.com>
2025-07-22 20:49:42 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-0c768662cc797cd75" # Ubuntu 22.04 (Mumbai)
   instance_type = var.instance_type
-  key_name      = "jenkinskey02" # optional
+  key_name      = "jenkins03" # optional
   user_data     = file("${path.module}/../../scripts/user_data.sh")
 
   tags = {
