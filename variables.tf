f56dc36c3c40e158175f05e0f5aba49221bf326e
Naveen Harikoppa <217095533+Naveen-5288@users.noreply.github.com>
2025-07-23 00:04:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "aws_secret_key" {}
 variable "instance_type" {
   default = "t2.micro"
 }
+variable "jenkins-key" {
+description = "The name of the AWS key pair to use for SSH"
+  type        = string
+}
