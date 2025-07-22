b70751ab49d657df8aa82412d5f827a0a8109e26
Naveen Harikoppa <217095533+Naveen-5288@users.noreply.github.com>
2025-07-23 00:00:53 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,5 +1,5 @@
 variable "instance_type" {}
-//variable "jenkins-key" {
- // description = "The name of the AWS key pair to use for SSH"
-  //type        = string
-//}
+variable "jenkins-key" {
+description = "The name of the AWS key pair to use for SSH"
+  type        = string
+}
