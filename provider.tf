5108f64f684c808119c097fab8a194a8b5fa3f05
Naveen Harikoppa <217095533+Naveen-5288@users.noreply.github.com>
2025-07-24 23:17:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = aws-access-key-id
-  secret_key = aws-secret-access-key
+  access_key = aws_access_key_01
+  secret_key = aws_secret_access_key_01
 }
