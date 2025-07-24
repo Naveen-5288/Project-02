246dedb7ebf23078000c6b95d78b2aafbcfc5bc8
Naveen Harikoppa <217095533+Naveen-5288@users.noreply.github.com>
2025-07-24 22:46:18 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,8 @@
 pipeline {
   agent any
   environment {
-    AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
-    AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+    AWS_ACCESS_KEY_ID     = credentials('aws-access-key')
+    AWS_SECRET_ACCESS_KEY = credentials('aws-secret-key')
     AWS_DEFAULT_REGION    = 'ap-south-1' // replace if using a different region
   }
   stages {
